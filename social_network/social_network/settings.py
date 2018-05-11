d4a83cb8cbb1553bdfc3787f841dff3d2fc5289f
Muhammed Magdy Taha <31701182+MuhammedMegz@users.noreply.github.com>
2018-05-11 23:58:35 +0200

    Update settings.py

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l2#ua8e#ucp8h=uu9wq)c_)+%g3w8zb49pp7nca#h52=-a=7yw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://muhammedmagdytaha.pythonanywhere.com/']
 
 
 # Application definition
